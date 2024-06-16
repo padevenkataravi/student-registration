be49748d4a09d661afff200710bee3799c803e26
ramanaiah-pade <ramanaiah.pade1921@gmail.com>
2024-06-16 15:43:19 +0530

    as per story I created student classes

--- src/main/java/com/ravi/studentrepository/StudentRepository.java
+++ src/main/java/com/ravi/studentrepository/StudentRepository.java
@@ -2,6 +2,6 @@ package com.ravi.studentrepository;
 
 public class StudentRepository {
     public static void main(String[] args) {
-        System.out.println("hhhhh");
+        System.out.println("Hello World");
     }
 }
